91700b57f5c8ed3595158a067a88e02de89cc37a
Thibault Notargiacomo <gnthibault@gmail.com>
2016-05-04 10:23:02 +0200

    enhancement for return value checking

--- coursTemplate/test.cpp
+++ coursTemplate/test.cpp
@@ -10,7 +10,9 @@ template<typename T>
 struct has_size
 {
   template<typename U>
-  static auto check( int ) -> decltype( std::declval<U>().size(), std::true_type() );
+  static auto check( int ) -> decltype( std::declval<U>().size(), //error if no size method on type U
+                                        size_t(std::declval<U>().size()), //error if return value of method size is not convertible in size_t
+                                        std::true_type() );
   //Pour créer un object factice pour lui prendre sa methode size, il faut une instance,
   //mais toutes les classes n'ont pas de constructeur, il y a un helper declval qui fourni
   //la garantie de renvoyer une instance
@@ -49,6 +51,10 @@ typename only_if< !has_size<C>::type::value, std::size_t>::type size(const C& c)
   return 1u;
 };
 
+struct Foo
+{
+  void size() {};
+};
 
 
 //g++ ./test.cpp -std=c++11 -o test
@@ -56,12 +62,14 @@ int main( int argc, char* argv[])
 {
   std::vector<double> n(8);
   float x;
+  Foo f{};
 
   //std::cout << std::boolalpha << has_size<float>::Type() << std::endl;
   //std::cout << std::boolalpha << has_size<std::vector<double>>::Type() << std::endl;
 
   std::cout << size(n) << std::endl;
   std::cout << size(x) << std::endl;
+  std::cout << size(f) << std::endl;
 
 
   return EXIT_SUCCESS;
